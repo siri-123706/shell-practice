490c8156aae12c5647d94f7d80061a804168c597
shirishakodari <kodarishirisha419@gmail.com>
2025-09-07 11:24:11 +0530

    conservation

---
+++ 02-conservation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "anu:: Hey siri, How are you?"
+echo "siri:: Hello anu, I am fine. How are you doing"
+echo "anu: I am fine too. What's up?"
+echo "siri:: Nohting, just going to Mars now, will you come?"
+echo "anu:: Sorry, you carry on! I will come once you come back"
